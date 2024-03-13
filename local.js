c89135f500b1135bd9a7b6a6fbd77da9f23b99e4
Yaroslav <ya.lepetchenko@onepointcollab.com>
2024-03-13 10:38:55 +0200

    ADD: localization

---
+++ local.js
@@ -0,0 +1,8 @@
+document.getElementById('app-title').textContent = chrome.i18n.getMessage('app_title');
+document.getElementById('register-link-text').innerText = chrome.i18n.getMessage('register_button');
+document.getElementById('sign-in-link-text').innerText = chrome.i18n.getMessage('sign_in_button');
+document.getElementById('google-link-text').innerText = chrome.i18n.getMessage('google_button');
+document.getElementById('make-call-button').innerText = chrome.i18n.getMessage('make_call_button');
+document.getElementById('stop-work-button').innerText = chrome.i18n.getMessage('stop_work_button');
+document.getElementById('start-work-button').innerText = chrome.i18n.getMessage('start_work_button');
+document.getElementById('input-itself').placeholder = chrome.i18n.getMessage('input_number_placeholder');
